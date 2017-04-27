12dafdd6ffb9212e99ccdcd2391d6b3275fd2b63
Tim <Tim>
2017-04-26 22:36:45 -0700

    functions

---
+++ if_else_elif.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+if (7<6) and (4>3):
+    print("if")
+elif (7>6):
+    print("elif")
+else:
+    print ("else")
+
+
+
+if (7) or (3>5):
+    print ("yep")
+else:
+    print("nope")
+
+
+
+if not (6):
+    print("yep")
+else:
+    print ("nope")
+
+
+
+
+
